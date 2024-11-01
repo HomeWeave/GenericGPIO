f6ce4b2272717c5f48cf40d35b15043a03fbc2c2
Srivatsan Iyer <supersaiyanmode.rox@gmail.com>
2024-11-01 21:15:37 +0530

    Bug fixes.

--- genericgpio/devices.py
+++ genericgpio/devices.py
@@ -100,7 +100,7 @@ class MotionSensorDevice(SimpleSensorDevice):
         msg = DeviceState(device_id=self.device_id())
         msg.motion_sensor_event = (MotionSensorState.MOTION_DETECTED
                                    if value else MotionSensorState.NO_MOTION)
-        self.send_event(event)
+        self.devices_manager.send_event(event)
 
     @staticmethod
     def new_config():
